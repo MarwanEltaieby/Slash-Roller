e0c1000f475d5de20f9203bed20ab58b07a54aba
DESKTOP-5LN6JE3\Nightmare <Marwaneltaieby@gmail.com>
2026-06-24 01:59:59 +0300

    Added the IMC and the IA and bind them in CPP + Added the Animals asset pack

---
+++ Source/Slash_Roller/Private/Pawns/Bird.cpp
@@ -0,0 +1,71 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Pawns/Bird.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "Components/InputComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+
+
+
+// Sets default values
+ABird::ABird()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Capsule = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule"));
+	Capsule->SetCapsuleHalfHeight(20.f);
+	Capsule->SetCapsuleRadius(15.f);
+	SetRootComponent(Capsule);
+
+	BirdMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("SkeletalMesh"));
+	BirdMesh->SetupAttachment(Capsule);
+	
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
+	
+}
+
+// Called when the game starts or when spawned
+void ABird::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	if (APlayerController* PlayerController = Cast<APlayerController>(GetController()))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			if (Subsystem && BirdMappingContext)
+			{
+				Subsystem->AddMappingContext(BirdMappingContext, 0);
+			}
+		}
+	}
+}
+
+void ABird::ProcessMovement(const FInputActionValue& Value)
+{
+	FVector2D MoveValue = Value.Get<FVector2D>();
+	UE_LOG(LogTemp, Warning, TEXT("MoveValue X: %f, Y: %f"), MoveValue.X, MoveValue.Y);
+}
+
+// Called every frame
+void ABird::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ABird::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	if (UEnhancedInputComponent* EnhancedInput = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInput->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ABird::ProcessMovement);
+	}
+}
+
